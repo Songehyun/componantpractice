3b650c2e1fceef5bafd6daa0e3ab9765be7387ca
송이현 <songehyun00@gmail.com>
2024-06-12 10:44:43 +0900

    work-1.js

---
+++ work-1.js
@@ -0,0 +1,14 @@
+const basicData = () => {
+  return {
+    name: "송이현",
+    age: 31,
+    job: "학생",
+  };
+};
+
+const component = () => {
+  let obj = basicData();
+
+  const decision = () => {};
+  return `<html></html>`;
+};
